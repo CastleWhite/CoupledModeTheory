d7e330b83d6849593d1a0512154ee0f8e0c057e1
CastleWhite <49804857+CastleWhite@users.noreply.github.com>
2021-01-25 10:06:03 +0800

    Add files via upload

---
+++ plot_by_mayavi.py
@@ -0,0 +1,9 @@
+import numpy as np
+from mayavi import mlab
+
+x, y = np.ogrid[-2:2:160j, -2:2:160j]
+z = abs(x) * np.exp(-(x ** 2) - (y / 0.75) ** 2)
+pl = mlab.surf(x, y, z, warp_scale=2)
+mlab.axes(xlabel="x", ylabel="y", zlabel="z")
+mlab.outline(pl)
+mlab.show()
